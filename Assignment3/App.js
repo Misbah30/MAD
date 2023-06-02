e51a71ba16bfe7bceebbc8c89102e8a95e82b39d
Misbah Nazir <113688643+Misbah30@users.noreply.github.com>
2023-06-02 22:41:31 +0500

    Update App.js

--- Assignment3/App.js
+++ Assignment3/App.js
@@ -1,1 +1,53 @@
 
+import * as React from 'react';
+import {View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from './LoginScreen';
+import SignupScreen from './SignupScreen';
+import Welcome from './WelcomeScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+   
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen}  options={{
+          title: 'Login Page',
+          headerStyle: {
+            backgroundColor: '#00203fff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}/>
+        <Stack.Screen name="Signup" component={SignupScreen}  options={{
+          title: 'Sign Up',
+          headerStyle: {
+            backgroundColor: '#00203fff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} />
+        <Stack.Screen name="wel" component={Welcome }  options={{
+          title: 'App',
+          headerStyle: {
+            backgroundColor: '#00203fff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
