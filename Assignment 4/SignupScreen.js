a5161d8d8a6c20cd041c5ecfd00c65e43be4580e
Misbah Nazir <113688643+Misbah30@users.noreply.github.com>
2023-06-11 17:11:42 +0500

    SignupScreen.js

--- Assignment 4/SignupScreen.js
+++ Assignment 4/SignupScreen.js
@@ -1,1 +1,110 @@
 
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const SignupScreen = ({ navigation }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignup = async () => {
+    if (password !== confirmPassword) {
+      alert('Error, Passwords do not match.');
+      return;
+    }
+
+    try {
+      const userData = await AsyncStorage.getItem('userData');
+      const users = userData ? JSON.parse(userData) : [];
+
+      const user = users.find(user => user.email === email);
+
+      if (user) {
+        alert('Error, Email already exists.');
+      } else {
+        const newUser = { name, email, password };
+        users.push(newUser);
+
+        await AsyncStorage.setItem('userData', JSON.stringify(users));
+        alert('Success, Signup successful. Please login.');
+        navigation.navigate('Login');
+      }
+    } catch (error) {
+      console.log('Error during signup:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Signup</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        onChangeText={text => setName(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={text => setPassword(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        secureTextEntry
+        onChangeText={text => setConfirmPassword(text)}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSignup}>
+        <Text style={styles.buttonText}>Signup</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#00203fff',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 8,
+    paddingHorizontal: 8,
+  },
+  button: {
+    backgroundColor: '#00203fff',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 4,
+    marginTop: 16,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default SignupScreen;
