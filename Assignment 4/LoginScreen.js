66530839e86593bcfc238edcef7dd34cd172d8e4
Misbah Nazir <113688643+Misbah30@users.noreply.github.com>
2023-06-11 17:10:08 +0500

    Update LoginScreen.js

--- Assignment 4/LoginScreen.js
+++ Assignment 4/LoginScreen.js
@@ -1,1 +1,95 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const userData = await AsyncStorage.getItem('userData');
+      const users = userData ? JSON.parse(userData) : [];
+
+      const user = users.find(user => user.email === email && user.password === password);
+
+      if (user) {
+        navigation.navigate('Welcome');
+      } else {
+        alert('Incorrect email or password.');
+      }
+    } catch (error) {
+      alert('Error during login:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={text => setPassword(text)}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+        <Text style={styles.textLink}>Don't have an account? Signup</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#00203fff',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 8,
+    paddingHorizontal: 8,
+  },
+  button: {
+    backgroundColor: '#00203fff',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 4,
+    marginTop: 16,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  textLink: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    marginTop: 16,
+  },
+});
+
+export default LoginScreen;
