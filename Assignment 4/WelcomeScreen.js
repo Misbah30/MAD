7e3ccfde8580b023db471f4cf7dc6291fd82651b
Misbah Nazir <113688643+Misbah30@users.noreply.github.com>
2023-06-11 17:12:40 +0500

    WelcomeScreen.js

--- Assignment 4/WelcomeScreen.js
+++ Assignment 4/WelcomeScreen.js
@@ -1,1 +1,33 @@
 
+import React from 'react';
+import { Button,View, Text, TextInput, StyleSheet,} from 'react-native';
+
+
+function Welcome(){
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome To My App</Text>
+      
+ 
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor:'white',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color:'#00203fff',
+  },
+  
+});
+export default Welcome;
